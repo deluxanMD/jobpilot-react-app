dad5ca0d611d5d113bac0b3dd4dd465a030fa74e
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-01-16 15:22:41 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-import Button from "./components/Button";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
